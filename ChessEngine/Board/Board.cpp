3d4379345972840dc735397dde6b6f12edb1f12c
Soham Deshmukh <131663742+SohamDeshmukh1@users.noreply.github.com>
2025-08-04 21:32:48 +0530

    Enums are good enough for them
    Reason is the Enum class needs to put effort in the the type conversion thats why enum is the best

--- ChessEngine/Board/Board.cpp
+++ ChessEngine/Board/Board.cpp
@@ -2,3 +2,9 @@
 
 int Sqr120to64[TOTAL_SQ];
 int Sqr64to120[64];
+
+void SQ120to64() {
+
+	int rank = RANK::RANK_1;
+
+}
--- ChessEngine/Board/Board.h
+++ ChessEngine/Board/Board.h
@@ -2,14 +2,12 @@
 #include<cstdint> //stdint.h is for c, cstdint this one is made for cpp 
 #include<iostream>
 #define TOTAL_SQ 120
-#define MAXMOVES 1048
+#define MAXMOVES 2048
 
 // I Hope i learn Stuff while Doing this
 
-class BOARD {
-public:
-	enum class SQUARE {
-		//Square Locations Present in the Board
+	enum SQUARE {
+		
 		A1 = 21, B1, C1, D1, E1, F1, G1, H1,
 		A2 = 31, B2, C2, D2, E2, F2, G2, H2,
 		A3 = 41, B3, C3, D3, E3, F3, G3, H3,
@@ -19,30 +17,29 @@ public:
 		A7 = 81, B7, C7, D7, E7, F7, G7, H7,
 		A8 = 91, B8, C8, D8, E8, F8, G8, H8
 	};
-	enum class RANK {
+	enum RANK {
 		// The Rank numbering of the board
 		RANK_1, RANK_2, RANK_3, RANK_4, RANK_5,
 		RANK_6, RANK_7, RANK_8, RANK_NONE
 	};
-	enum class PIECE {EMPTY, // All The Pieces of thge Board 	
+	enum PIECE {EMPTY, // All The Pieces of thge Board 	
 		Wp, Wr, Wb, Wn, Wk, Wq,
 		Bp, Br, Bb, Bn, Bk, Bq
 	};
-	enum class FILE { // Similar to the Ranks but The Alphabets 
+	enum FILE { // Similar to the Ranks but The Alphabets 
 		FILE_A, FILE_B, FILE_C,
 		FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE
 	};
-	enum class TEAM { // Idk to keep the Count of the Teams maybe?
+	enum TEAM { // Idk to keep the Count of the Teams maybe?
 		BLACK, WHITE, BOTH
 	};
-	enum class CASTLING { // Defining Castling perm white king/queen and Smiliary for Black too
+	enum CASTLING { // Defining Castling perm white king/queen and Smiliary for Black too
 		WKCA = 1 , WQCA = 2 , BKCA = 4 , BQCA = 8
 	};
 	bool State = true;
-};
 
 typedef struct {
-	// We need ummm place for Pieces
+
 	
 	int piece[TOTAL_SQ];
 	std::uint64_t Pawn[3]; //To track Pawns
@@ -76,16 +73,13 @@ typedef struct {
 	std::uint64_t PosKey;
 } S_Undo;
 
-S_Undo History[MAXMOVES];  // Never though it would in the way like every array index
-//holds all these elements never saw that coming (just my thought)
+S_Undo History[MAXMOVES]; 
 
 /*  ---------- GLOBALS -----------  */
 
 extern int Sqr120to64[TOTAL_SQ];
-extern int Sqr64to120[64];
-
-/*  ---------- Macros -----------  */
+extern int Sqr64to120[64];  
+/*  ---------- Macros  -----------  */
 
 #define FR2SQ(f, r) ( (21 + (f)) + ((r) * 10) )
-// He has a good namig Convention 
-// i was about to name it SqrPos but it still sounds same i guess?
+// Returns 120 SQ value
