7483a235c4d5cbd253a30a27eb3165fcf69ea798
Soham Deshmukh <131663742+SohamDeshmukh1@users.noreply.github.com>
2025-08-04 22:11:49 +0530

    Absolute CINEMA
    i was pretty impressed by this logic i dont know if its me but yeah i am dayum sure i want to learn something like this

--- ChessEngine/Board/Board.cpp
+++ ChessEngine/Board/Board.cpp
@@ -5,6 +5,25 @@ int Sqr64to120[64];
 
 void SQ120to64() {
 
-	int rank = RANK::RANK_1;
+	int rank = RANK_1;
+	int file = FILE_A;
+	int index = 0;
+	int sqr = A1;
+	int sqr64 = 0;
 
+	for (index = 0; index < TOTAL_SQ; ++index) {
+		Sqr120to64[index] = 65;
+	}
+	for (index = 0; index < 64; ++index) {
+		Sqr64to120[index] = 120;
+	}
+
+	for (rank = RANK_1; rank <= RANK_8; ++rank) {
+		for (file = FILE_A; file <= FILE_H; ++file) {
+			sqr = FR2SQ(file, rank);
+			Sqr64to120[sqr64] = sqr;
+			Sqr120to64[sqr] = sqr64;
+			sqr64++;
+		}
+	}
 }
